0d9975187a7dd79c705f39b395f0176ba9aee285
youngjung <youngjung.research@gmail.com>
2023-05-08 13:56:10 +0900

    added zebra

--- animal.py
+++ animal.py
@@ -10,3 +10,7 @@ class Animal:
     def breed(self, x, y):
         print(f'breed to {x}, {y}. <<< NOT IMPLEMENTED YET >>>')
         # return Animal(x, y)
+
+class Zebra(Animal):
+    def move(self, occupancy_grid):
+        print('<<< NOT IMPLEMENTED >>>')
